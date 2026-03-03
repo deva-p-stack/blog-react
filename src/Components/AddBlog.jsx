4f041315d987de7fd1982f82a51bb2318c0eae96
deva-p-stack <devap677@gmail.com>
2026-03-03 10:22:24 +0530

    FIX: froms layout issues;

--- src/Components/AddBlog.jsx
+++ src/Components/AddBlog.jsx
@@ -52,19 +52,19 @@ return(
             
 
 
-            <div className="flex flex-col py-1 space-x-3 ">
+            <div className="flex flex-col py-1 space-x-3 space-y-0.5 ">
                 <label className="text-xs" htmlFor="name">Blog Title</label>
                 <input onChange={(e)=> setBlog({...blog,title: e.target.value})} className="bg-red-400 outline-none rounded-lg py-2 px-3" type="text" name="name" id="name" aria-label="name" />
             </div>
 
-             <div className="flex flex-col py-1 space-x-3 ">
+             <div className="flex flex-col py-1 space-x-3 space-y-0.5">
                 <label className="text-xs" htmlFor="date">Publish Date</label>
                 <input className="bg-red-400 outline-none rounded-lg py-2 px-3" type="date" name="date" id="date" aria-label="date" />
             </div>
 
 
          
-            <div className="flex flex-col pt-1.5 space-x-3 ">
+            <div className="flex flex-col pt-1.5 space-x-3 space-y-0.5 ">
                 <label className="text-xs" htmlFor="name">Category</label>
                 <select onChange={(e)=> setBlog({...blog,category: e.target.value})} className="py-2 px-3 bg-red-400 outline-none rounded" name="Category" id="Category">
                 <option value="">Web Development</option>
@@ -77,32 +77,32 @@ return(
             </div>  
 
 
-            <div className="flex flex-col py-1 space-x-3 ">
+            <div className="flex flex-col py-1 space-x-3 space-y-0.5">
                 <label className="text-xs" htmlFor="authorname">Author Name</label>
                 <input onChange={(e)=> setBlog({...blog,authorName: e.target.value})}  className="bg-red-400 outline-none rounded-lg py-2 px-3" type="text" name="authorname" id="authorname" aria-label="authorname" />
             </div>
 
-             <div className="flex flex-col py-1 space-x-3 ">
+             <div className="flex flex-col py-1 space-x-3 space-y-0.5 ">
                 <label className="text-xs" htmlFor="tags">Tags</label>
                 <input onChange={(e)=> setBlog({...blog,tags: e.target.value})} className="bg-red-400 outline-none rounded-lg py-2 px-3" type="text" name="tags" id="tags" aria-label="tags" />
             </div>
 
             
-             <div className="flex flex-col py-1 space-x-3 ">
+             <div className="flex flex-col py-1 space-x-3 space-y-0.5 ">
                 <label className="text-xs" htmlFor="tags">Status</label>
                 <input onChange={(e)=> setBlog({...blog,status: e.target.value})} className="bg-red-400 outline-none rounded-lg py-2 px-3" type="text" name="status" id="status" aria-label="status" />
             </div>
            
 
-                  <div className="flex flex-col col-span-full py-1 space-x-3  ">
+                  <div className="flex flex-col col-span-full py-1 space-x-3 space-y-0.5 ">
                 <label className="text-xs" htmlFor="Blog Content">Blog Content</label>
                 <textarea 
                 onChange={(e)=> setBlog({...blog,blogContent: e.target.value})} className="bg-red-400 outline-none rounded-lg py-2 px-3" type="textarea" name="Blog Content" id="Blog Content" aria-label="Blog Content">
                 </textarea>
 
 
-                <div className=" flex justify-center items-center pt-3">
-                    <button onClick={handledata} className="px-3 py-2 hover:bg-red-300 bg-red-400 rounded  cursor-pointer active:scale-95 transition-all ease-in-out duration-300 "  type="submit">
+                <div className=" flex justify-center items-center py-3">
+                    <button onClick={handledata} className="px-3 w-[50%] py-2 hover:bg-red-300 bg-red-400 rounded  cursor-pointer active:scale-95 transition-all ease-in-out duration-300 "  type="submit">
 
 
 
